1dd44f22ed4266da36bffb8464a33ff9b4f774d3
maciejtarsa <maciej.tarsa@mechanicalrock.io>
2025-07-01 08:39:23 +0000

    fixes and breaking a model

--- dbt/models/marts/orders.sql
+++ dbt/models/marts/orders.sql
@@ -30,7 +30,8 @@ SELECT
     oh.order_amount,
     oh.order_tax_amount,
     oh.order_discount_amount,
-    oh.order_total
+    oh.order_total,
+    oh.order_failed
 FROM {{ ref('raw_pos_order_detail') }} od
 JOIN {{ ref('raw_pos_order_header') }} oh
     ON od.order_id = oh.order_id
