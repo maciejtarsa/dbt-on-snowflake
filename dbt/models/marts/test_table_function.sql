2d79c5a6f1fe848bc90c4cd54c706d3edb02ac98
Maciej Tarsa <maciej.tarsa@mechanicalrock.io>
2025-07-09 15:50:36 +0800

    fix

--- dbt/models/marts/test_table_function.sql
+++ dbt/models/marts/test_table_function.sql
@@ -3,5 +3,5 @@ with source_data as (
     from {{ ref('raw_customer_customer_loyalty') }}
 )
 
-SELECT transform_table.id, value
+SELECT transform_table.id, transform_table.new_value
   FROM source_data, TABLE(DBT_DEMO.DEV.transform_table(first_name, city))
